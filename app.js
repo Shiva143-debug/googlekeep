3acc59bc63953adfc21e867f23bbff37dbcaccc6
Ramagiri shiva shankara prasad <shivarama311@gmail.com>
2024-07-20 13:19:43 +0530

    added spinner for late loading

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 
-
 document.addEventListener('DOMContentLoaded', () => {
   const registerForm = document.getElementById('register-form');
   const loginForm = document.getElementById('login-form');
@@ -22,17 +21,12 @@ document.addEventListener('DOMContentLoaded', () => {
   Trash.addEventListener('click', onTrashClick);
   note.addEventListener('click', onNoteClick);
   archeive.addEventListener('click', onArchieveClick);
-
   directLogin.addEventListener('click', onDirectLoginClick);
   directRegister.addEventListener('click', onDirectRegisterClick);
-
   registerForm.addEventListener('submit', onRegisterFormSubmit);
   loginForm.addEventListener('submit', onLoginFormSubmit);
-
   createNoteButton.addEventListener('click', onCreateNoteButtonClick);
-
   searchInput.addEventListener('input', onSearchInputChange);
-
   logoutButton.addEventListener('click', onLogoutButtonClick);
 
     note.style.border = '1px solid black'
@@ -105,13 +99,11 @@ document.addEventListener('DOMContentLoaded', () => {
     const registerButton = document.getElementById('register-button');
     const spinner = document.createElement('div');
     spinner.classList.add('spinner');
-
     registerButton.textContent = '';
     registerButton.appendChild(spinner);
     const username = document.getElementById('register-username').value;
     const email = document.getElementById('register-email').value;
     const password = document.getElementById('register-password').value;
-
     const response = await fetch('https://leeward-walnut-wavelength.glitch.me/register', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
@@ -137,12 +129,10 @@ document.addEventListener('DOMContentLoaded', () => {
     const loginButton = document.getElementById('login-button');
     const spinner = document.createElement('div');
     spinner.classList.add('spinner');
-
     loginButton.textContent = '';
     loginButton.appendChild(spinner);
     const email = document.getElementById('login-email').value;
     const password = document.getElementById('login-password').value;
-
     const response = await fetch('https://leeward-walnut-wavelength.glitch.me/login', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
@@ -176,7 +166,7 @@ document.addEventListener('DOMContentLoaded', () => {
         });
 
         if (response.ok) {
-          await fetchNotes(); // Fetch all notes including the new one
+          await fetchNotes(); 
         } else {
           alert('Failed to add note. Please try again.');
         }
@@ -225,11 +215,7 @@ document.addEventListener('DOMContentLoaded', () => {
       if (notes.length === 0) {
         TrashDiv.innerHTML = '<p class="no-notes" style="text-align: center; padding-left:500px">No Trash notes yet!</p>';
       }
-      // TrashDiv.innerHTML = 'Notes in Trash will automatically delete after 30 days';
-      // TrashDiv.append(TrashDiv.innerHTML)
-
       notes.forEach(note => {
-
         const noteElement = createNoteElement(note, "trash");
         TrashDiv.appendChild(noteElement);
       });
@@ -307,34 +293,24 @@ document.addEventListener('DOMContentLoaded', () => {
       const img = document.createElement('img');
       img.src = note.imageurl;
       img.alt = 'Note Image';
-      
-      // Set styles for the image (e.g., max width to ensure it fits within the note)
-      img.style.maxWidth = '100%'; // Adjust as needed
-      img.style.height = 'auto';   // Maintain aspect ratio
-  
-      // Append the image below the content area
+      img.style.maxWidth = '100%'; 
+      img.style.height = 'auto';  
       noteElement.appendChild(img);
     }
-    // Create editable content area
     const contentArea = document.createElement('div');
     contentArea.contentEditable = true;
     contentArea.textContent = note.content;
-    contentArea.style.minHeight = '200px'; // Example height
-
-    // Apply styling to note element
+    contentArea.style.minHeight = '200px'; 
     noteElement.style.width = '250px';
     noteElement.style.minHeight = '250px';
     noteElement.style.padding = '10px';
     noteElement.style.border = '1px solid #ccc';
     noteElement.style.borderRadius = '8px';
     noteElement.style.margin = '10px';
-
-    noteElement.style.position = 'relative'; // Ensure positioning for icons
+    noteElement.style.position = 'relative'; 
     noteElement.style.display = 'flex';
     noteElement.style.flexDirection = 'column';
-    noteElement.style.justifyContent = 'space-between'; // Align icons at the bottom
-
-    // Create delete icon
+    noteElement.style.justifyContent = 'space-between';
     const deleteIcon = document.createElement('svg');
     deleteIcon.classList.add('bi', 'bi-trash');
     deleteIcon.setAttribute('width', '1em');
@@ -348,7 +324,7 @@ document.addEventListener('DOMContentLoaded', () => {
     `;
     deleteIcon.style.cursor = 'pointer';
     deleteIcon.addEventListener('click', async () => {
-      const confirmed = confirm(`Are you sure you want to delete this ${name}note?`);
+      const confirmed = confirm(`Are you sure you want to delete this ${name} note?`);
       if (confirmed) {
         try {
           const id = parseInt(noteElement.dataset.id);
@@ -374,8 +350,6 @@ document.addEventListener('DOMContentLoaded', () => {
         }
       }
     });
-
-    // Create archive icon
     
       const archiveIcon = document.createElement('svg');
       archiveIcon.classList.add('bi', 'bi-archive');
@@ -424,26 +398,18 @@ document.addEventListener('DOMContentLoaded', () => {
         <path fill-rule="evenodd" d="M2.5 1.5a1 1 0 0 1 1-1h8a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1h-8a1 1 0 0 1-1-1v-3zm1 1v2h2V2.5h-2zm4 0v2h2V2.5h-2zm-4 3v2h2V5.5h-2zm4 0v2h2V5.5h-2zm-4 3v2h2V8.5h-2zm4 0v2h2V8.5h-2zm-4 3v2h2v-2h-2zm4 0v2h2v-2h-2z"/>
     `;
     colorPaletteIcon.style.cursor = 'pointer';
-    // Create a new input element for color picking
     const colorPicker = document.createElement('input');
     colorPicker.setAttribute('type', 'color');
     colorPicker.setAttribute('id', 'colorPicker');
     colorPicker.setAttribute('name', 'colorPicker');
-    colorPicker.style.display = 'none'; // Hide the color picker initially
-
-    // Append the color picker input to the body or another container
-    document.body.appendChild(colorPicker); // Adjust container as per your layout
-
-    // Event listener for the colorPaletteIcon button click
+    colorPicker.style.display = 'none'; 
+    document.body.appendChild(colorPicker); 
     colorPaletteIcon.addEventListener('click', () => {
-      // Display the color picker
-      colorPicker.click(); // This opens the color picker dialog
+      colorPicker.click(); 
     });
-
-    // Event listener for when a color is picked
     colorPicker.addEventListener('input', async () => {
 
-      const selectedColor = colorPicker.value; // Get the selected color
+      const selectedColor = colorPicker.value; 
       const id = parseInt(noteElement.dataset.id);
       const response = await fetch(`https://leeward-walnut-wavelength.glitch.me/${name}background/${id}`, {
         method: 'PUT',
@@ -461,18 +427,8 @@ document.addEventListener('DOMContentLoaded', () => {
       } else if (name === "archeive") {
         fetchArchieveItems();
       }
-
-
-
-      // Apply the selected color to the note background (example)
-      // const note = document.querySelector('.note'); // Adjust this selector as per your note's structure
-      // note.style.backgroundColor = selectedColor;
     });
 
-
-
-
-    // Create image upload icon
     const imageIcon = document.createElement('svg');
     imageIcon.classList.add('bi', 'bi-image');
     imageIcon.setAttribute('for', `imageInput_${note.id}`);
@@ -485,18 +441,14 @@ document.addEventListener('DOMContentLoaded', () => {
         <path fill-rule="evenodd" d="M3.5 1.5a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5V3h-11V1.5zm7.5 2.5a2 2 0 0 1 2 2v5.793l-2-2V5.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v3.293l-2 2V6a2 2 0 0 1 2-2h3zm1 .5H11a1 1 0 0 0 1-1V5a2 2 0 0 0-2-2h-1a2 2 0 0 0-2 2v1a1 1 0 0 0 1 1h1a.5.5 0 0 1 .5.5v3.793l.646-.647a.5.5 0 0 1 .708 0l1.646 1.647V6a1 1 0 0 0-1-1z"/>
         <path d="M1.707 4.293a1 1 0 0 1 0-1.414l2-2a1 1 0 0 1 1.414 0l1.793 1.793 5.793 5.793a1 1 0 0 1 0 1.414l-2 2a1 1 0 0 1-1.414 0L7 9.414l-5.293-5.293a1 1 0 0 1 0-1.414z"/>
     `;
-    imageIcon.style.cursor = 'pointer';
-
-   
+    imageIcon.style.cursor = 'pointer';  
     const fileInput = document.createElement('input');
     fileInput.setAttribute('id', `imageInput_${note.id}`);
     fileInput.setAttribute('type', 'file');
     fileInput.setAttribute('accept', 'image/*');
-    fileInput.style.display = 'none'; // Hide input by default
-    
-    // Add event listener to image icon to trigger file input click
+    fileInput.style.display = 'none'; 
     imageIcon.addEventListener('click', () => {
-        fileInput.click(); // Programmatically trigger the file input
+        fileInput.click(); 
     });
     fileInput.addEventListener('change', async (event) => {
       const file = event.target.files[0];
@@ -513,25 +465,15 @@ document.addEventListener('DOMContentLoaded', () => {
   
               if (response.ok) {
                   const data = await response.json();
-  
-                  // Ensure imageUrl is defined and starts with 'data:image/'
                   const imageUrl = data.imageUrl;
                   if (imageUrl && imageUrl.startsWith('data:image/')) {
-                      // Create an <img> element and set its src to the Base64 URL
                       const img = document.createElement('img');
                       img.src = imageUrl;
                       img.alt = 'Uploaded Image';
-  
-                      // Optionally set styles for the image
-                      img.style.maxWidth = '100%'; // Example style
-                      img.style.height = 'auto';   // Example style
-  
-                      // Clear any existing images or content (if needed)
-                      const existingIconsContainer = noteElement.querySelector('.icons-container');
-                     
-                      noteElement.appendChild(img);
-
-                     
+                      img.style.maxWidth = '100%';
+                      img.style.height = 'auto';
+                      const existingIconsContainer = noteElement.querySelector('.icons-container');                    
+                      noteElement.appendChild(img);                    
                   } else {
                       alert('Invalid image URL received.');
                   }
@@ -544,13 +486,11 @@ document.addEventListener('DOMContentLoaded', () => {
           }
       }
   });
-  
-    
-    // Container for icons
+
     const iconsContainer = document.createElement('div');
     iconsContainer.style.display = 'flex';
-    iconsContainer.style.justifyContent = 'space-around'; // Align icons to the right
-    iconsContainer.style.marginTop = 'auto'; // Push icons to the bottom
+    iconsContainer.style.justifyContent = 'space-around'; 
+    iconsContainer.style.marginTop = 'auto'; 
     iconsContainer.appendChild(deleteIcon);
     if (name === "note"){
       iconsContainer.appendChild(archiveIcon);
@@ -560,15 +500,12 @@ document.addEventListener('DOMContentLoaded', () => {
     if (name === "note"){
     iconsContainer.appendChild(imageIcon)
     }
-    // Append content area and icons container to note element
     noteElement.appendChild(contentArea);
     noteElement.appendChild(iconsContainer);
-
-    // Handle click to edit note content
     contentArea.addEventListener('keydown', (event) => {
       if (event.key === 'Enter') {
         event.preventDefault();
-        contentArea.blur(); // Lose focus to trigger update
+        contentArea.blur(); 
       }
     });
 
@@ -585,7 +522,6 @@ document.addEventListener('DOMContentLoaded', () => {
         });
 
         if (response.ok) {
-          // Update UI or handle success
         } else {
           alert('Failed to update note content. Please try again.');
         }
